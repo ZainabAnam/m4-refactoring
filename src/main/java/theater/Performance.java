d7b1c566497135e764af4ebe27238d3ec7225f1f
Zainab Anam <zainab.anam@mail.utornoto.ca>
2025-11-15 11:48:30 -0500

    Task 1.1 completed

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,34 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance in the theater, with a playID (str) and audience (int).
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    // playID getter and setter
+    public String getPlayID() {
+        return playID;
+    }
+
+    public void setPlayID(String playID) {
+        this.playID = playID;
+    }
+
+    // audience getter and setter
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
+        this.audience = audience;
+    }
+
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,33 @@
 package theater;
 
+/**
+ * Class representing a play, with a name (str) and genre (str).
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
+        this.type = type;
+    }
+
+    // name getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // type getter and setter
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
